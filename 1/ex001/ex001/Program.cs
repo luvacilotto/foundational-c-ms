3682f4bd6d8b98720da60285636ddeb6509b33e4
Luísa Vacilotto <luvacilotto@gmail.com>
2024-03-26 19:32:02 -0300

    First exercise

---
+++ 1/ex001/ex001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World");
+            // Console.WriteLine imprime a saída na linha atual e acrescenta uma nova linha após ela.
+
+            //Console.Write("Congratulations!");
+            //Console.Write(" ");
+            //Console.Write("You wrote your first lines of code.");
+
+
+            // Desafio:
+            Console.WriteLine("This is the first line.");
+            Console.Write("This is the ");
+            Console.Write("second line.");
+
+        }
+    }
+}
